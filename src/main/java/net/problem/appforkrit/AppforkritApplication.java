a77ca658d4e4ece71f0313e4b156e2bd1595df1b
silabro <48855463+silabro@users.noreply.github.com>
2021-08-17 11:38:47 +0300

    Project initialization

---
+++ src/main/java/net/problem/appforkrit/AppforkritApplication.java
@@ -0,0 +1,13 @@
+package net.problem.appforkrit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppforkritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppforkritApplication.class, args);
+    }
+
+}
