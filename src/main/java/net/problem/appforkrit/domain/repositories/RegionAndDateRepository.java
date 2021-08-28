40ffcd69bc6ffd41534a978d83614129bbea110c
Pasha Shevtsev <pashkamen4ik@yandex.ru>
2021-08-28 18:36:26 +0300

    Added method 'findByNumberRegionAndDate' in RegionAndDateRepository

--- src/main/java/net/problem/appforkrit/domain/repositories/RegionAndDateRepository.java
+++ src/main/java/net/problem/appforkrit/domain/repositories/RegionAndDateRepository.java
@@ -5,7 +5,10 @@ import net.problem.appforkrit.domain.entities.RegionAndDateEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface RegionAndDateRepository extends CrudRepository<RegionAndDateEntity, Long> {
 
+    RegionAndDateEntity findByNumberRegionAndDate(short numberRegion, Date date);
 }
